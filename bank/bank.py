0a871104e2dcaf1eb28f9ddc54120c7188913691
lchamiria <lchamiria01@gmail.com>
2025-08-24 07:01:29 +0000

    Sun, Aug 24, 2025, 9:01 AM +02:00

--- bank/bank.py
+++ bank/bank.py
@@ -1,2 +1,10 @@
 greeting = input("greeting: ")
-def 
+def greeting():
+    for char in greeting:
+        if greeting.startswith("Hello"):
+            return "$0"
+        elif greeting.startswith("H"):
+            return "$20"
+        else:
+            return "$100"
+
