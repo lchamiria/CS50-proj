213a7283431dd32bdb49daea553b70e7839e8965
lchamiria <lchamiria01@gmail.com>
2025-08-24 06:30:01 +0000

    Sun, Aug 24, 2025, 8:30 AM +02:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,1 +1,7 @@
 user_input = input("What is the answer to the Great Question of Life, the Universe and Everything? ")
+if user_input == "42" or user_input == "forty-two" or user_input == "forty two":
+    return "Yes"
+else:
+    return "No"
+
+print(user_input)
