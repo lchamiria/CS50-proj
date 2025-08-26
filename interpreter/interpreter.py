00818ece530f3ecf610753a579b89a3707f94d94
lchamiria <lchamiria01@gmail.com>
2025-08-26 22:16:29 +0000

    Wed, Aug 27, 2025, 12:16 AM +02:00

---
+++ interpreter/interpreter.py
@@ -0,0 +1,13 @@
+x,y,z = input("Expression: ")
+x = int(x)
+z = int(z)
+
+match y:
+    case "/":
+        print(x/z)
+    case "*":
+        print(x*z)
+    case "-":
+        print(x-z)
+    case "+":
+        print(float(x+z))
