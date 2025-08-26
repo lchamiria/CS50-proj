dc3bc99609970ca44b0219b20017d74270141cb3
lchamiria <lchamiria01@gmail.com>
2025-08-26 18:04:11 +0000

    Tue, Aug 26, 2025, 8:04 PM +02:00

--- bank/bank.py
+++ bank/bank.py
@@ -1,13 +1,12 @@
 greeting = input("greeting: ").title().strip()
-def greetings():
-    for char in greeting:
-        if greeting.startswith("Hello"):
-            print("$0")
-        elif greeting.startswith("H"):
-            print("$20")
-        else:
-            print("$100")
 
-greetings()
+if greeting.startswith("Hello"):
+    print("$0")
+elif greeting.startswith("H"):
+    print("$20")
+else:
+    print("$100")
+
+
 
 
