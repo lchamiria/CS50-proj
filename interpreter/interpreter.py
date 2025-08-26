c2d183bcb26eba4d06b5199e0ad53ecae7f456de
lchamiria <lchamiria01@gmail.com>
2025-08-26 21:45:44 +0000

    Tue, Aug 26, 2025, 11:45 PM +02:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,11 +1,17 @@
 x,y,z = input("Expression: ")
 x = float(x)
 z = float(z)
-if y == "+":
-    print(x+z)
-elif y == "-":
-    print(x-z)
-elif y == "-":
-    print(x-z)
-elif y == "-":
-    print(x-z)
+for y in x,y,z:
+    if y == "+":
+        print(x+z)
+    elif y == "-":
+        print(x-z)
+    elif y == "*":
+        print(x*z)
+    elif y == "-":
+        print(x/z)
+    else:
+        break
+
+print(x,y,z)
+
