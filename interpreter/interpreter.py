2336d73be54e44b6598031b8381df9f08d464dab
lchamiria <lchamiria01@gmail.com>
2025-08-26 21:10:13 +0000

    Tue, Aug 26, 2025, 11:10 PM +02:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,8 +1,8 @@
 expression = input("Expression: ")
-x,y,z = expression.split(" ")
 x = str("x")
 y = "-", "+", "*", "/"
 z = str("z")
+x,y,z = expression.split(" ")
 expression = float(x,y,z)
 
 print(expression)
