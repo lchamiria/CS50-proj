18231c2b4b8d6ac1f3a9150a9157c182fe56975a
lchamiria <lchamiria01@gmail.com>
2025-08-26 21:51:43 +0000

    Tue, Aug 26, 2025, 11:51 PM +02:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -2,16 +2,3 @@ x,y,z = input("Expression: ")
 x = int(x)
 z = int(z)
 for y in x,y,z:
-    if y == "+":
-        return(x+z)
-    elif y == "-":
-        return(x-z)
-    elif y == "*":
-        return(x*z)
-    elif y == "-":
-        return(x/z)
-    else:
-        break
-
-print(x,y,z)
-
