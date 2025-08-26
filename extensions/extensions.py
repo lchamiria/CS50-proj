393cb7f72114becb745b705872a8699cb50180ff
lchamiria <lchamiria01@gmail.com>
2025-08-26 18:48:08 +0000

    Tue, Aug 26, 2025, 8:48 PM +02:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -2,5 +2,24 @@ file_name = input("File name: ")
 
 if file_name.endswith(".gif"):
     print("image/gif")
-elif file_name.endswith(.)
 
+elif file_name.endswith(.jpg):
+    print("image/jpg")
+
+elif file_name.endswith(".jpeg"):
+    print("image/jpeg")
+
+elif file_name.endswith(".png"):
+    print("image/png")
+
+elif file_name.endswith(".pdf"):
+    print("image/pdf")
+
+elif file_name.endswith(".txt"):
+    print("image/txt")
+
+elif file_name.endswith(".zip"):
+    print("image/zip")
+
+else:
+    print("application/octet-stream")
