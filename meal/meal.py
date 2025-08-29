5042f1696aa5e1e1ef2aa86a4dcb25be75ba6a82
lchamiria <lchamiria01@gmail.com>
2025-08-29 18:00:54 +0000

    Fri, Aug 29, 2025, 8:00 PM +02:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,15 +1,16 @@
 def main():
     time = input("What time is it? ")
-    hours,minutes = time.split(":")
-
-
-def convert(time):
     if time >= 7 and time <= 8:
         print("breakfast time")
     elif time >= 12 and time <= 13:
         print("lunch time")
     elif time >= 18 and time <= 19:
         print("dinner time")
+    hours,minutes = time.split(":")
+
+
+def convert(time):
+
 
 
 
