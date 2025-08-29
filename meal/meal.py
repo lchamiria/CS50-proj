7ad216b89280e4864d08ee7ada312526cd7e7ea2
lchamiria <lchamiria01@gmail.com>
2025-08-29 17:24:28 +0000

    Fri, Aug 29, 2025, 7:24 PM +02:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,9 +1,13 @@
 def main():
-    ...
+    time = input("What time is it? ")
 
 
 def convert(time):
-    ...
+    if time >= 7 and time <= 8:
+        print("breakfast time")
+    elif time >= 12 and time <= 13:
+        print("lunch time")
+        
 
 
 if __name__ == "__main__":
